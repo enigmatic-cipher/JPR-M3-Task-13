88d5f7d0f940606fefd4e9aa8d8fedc24e87bd42
Amit <87896992+enigmatic-cipher@users.noreply.github.com>
2022-01-30 13:05:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+Given an array sorted in ascending array, create a new array where the elements have been sorted in descending order.
+Input-> [1,2,3,4,5]
+Output-> [5,4,3,2,1]
+"""
+
+ls = [1,2,3,4,5][::-1]
+print(ls)
